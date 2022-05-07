f284e4d539a922e54b8525f4e9a197dcb72d3e29
WillMeagher <willfmeagher@gmail.com>
2022-05-07 14:56:50 -0400

    Fixed merge issue

--- app/src/main/java/com/example/smarternships/ui/account/EditAccountActivity.kt
+++ app/src/main/java/com/example/smarternships/ui/account/EditAccountActivity.kt
@@ -12,10 +12,6 @@ import com.example.smarternships.R
 import com.example.smarternships.data.model.DataBase
 import com.example.smarternships.data.model.OnGetDataListener
 import com.example.smarternships.data.model.User
-<<<<<<< HEAD
-=======
-import com.example.smarternships.ui.job.ViewJobActivity
->>>>>>> a51aee7d74e65d2c74db7923a8609772fb1148ef
 import com.google.firebase.database.DataSnapshot
 
 class EditAccountActivity : AppCompatActivity() {
@@ -24,7 +20,6 @@ class EditAccountActivity : AppCompatActivity() {
     private lateinit var mEmailView: EditText
     private lateinit var mDescriptionView: EditText
     private lateinit var mSaveButton: Button
-    lateinit var user: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,11 +29,7 @@ class EditAccountActivity : AppCompatActivity() {
         val i = intent
         val e = i.extras
 
-<<<<<<< HEAD
         val userID = e?.getString("USERID");
-=======
-        val userID = b?.getString("USERID");
->>>>>>> a51aee7d74e65d2c74db7923a8609772fb1148ef
 
         mEditNameView = findViewById<View>(R.id.edit_name) as EditText
         mEmailView = findViewById<View>(R.id.edit_email) as EditText
@@ -50,23 +41,17 @@ class EditAccountActivity : AppCompatActivity() {
 
             DataBase.getUser(userID, object : OnGetDataListener {
                 override fun onSuccess(dataSnapshot: DataSnapshot?) {
-<<<<<<< HEAD
                     var user = dataSnapshot?.getValue(User::class.java)
-=======
                     user = dataSnapshot?.getValue(User::class.java)!!
->>>>>>> a51aee7d74e65d2c74db7923a8609772fb1148ef
                     if (user != null) {
                         mEditNameView.setText(user.userName)
                         mEmailView.setText(user.userEmail)
                         mDescriptionView.setText(user.userDescription)
-<<<<<<< HEAD
                         if(user.isIntern) {
                             mDescriptionView.hint = "Resume"
                         } else {
                             mDescriptionView.hint = "Company Description"
                         }
-=======
->>>>>>> a51aee7d74e65d2c74db7923a8609772fb1148ef
                     }
                 }
 
@@ -103,16 +88,14 @@ class EditAccountActivity : AppCompatActivity() {
                 }
 
             }else{
-<<<<<<< HEAD
                 if (userID != null) {
                     DataBase.getUser(userID, object : OnGetDataListener {
                         override fun onSuccess(dataSnapshot: DataSnapshot?) {
-                            var user = dataSnapshot?.getValue(User::class.java)
+                            val user = dataSnapshot?.getValue(User::class.java)
                             if (user != null) {
                                 user.userName = mNameString
                                 user.userDescription = mDescriptionString
                                 DataBase.setUser(userID, user)
-
                             }
                         }
 
@@ -129,24 +112,6 @@ class EditAccountActivity : AppCompatActivity() {
                 val intent = Intent(this, ViewAccountActivity::class.java)
                 intent.putExtra("USERID", userID)
                 startActivity(intent)
-=======
-                val userID = b?.getString("USERID")
-                var editedUser = userID?.let{ it -> User(
-                    userName = mNameString,
-                    userEmail = mEmailString,
-                    userDescription = mDescriptionString,
-                    isIntern = user.isIntern,
-                    currentJobs = user.currentJobs,
-                    completedJobs = user.completedJobs
-                )}
-
-                if(editedUser != null){
-                    DataBase.createUser(userID!!,editedUser)
-                    val intent = Intent(this, ViewAccountActivity::class.java)
-                    intent.putExtra("USERID", userID)
-                    startActivity(intent)
-                }
->>>>>>> a51aee7d74e65d2c74db7923a8609772fb1148ef
             }
         }
     }
